400931459bdcf59b72e47ac4918704f685410ce0
toda93 <dttong93@gmail.com>
2020-03-03 22:17:22 +0700

    sync

--- src/Provider.js
+++ src/Provider.js
@@ -1,4 +1,5 @@
 import RabbitMQAsync from './RabbitMQAsync';
+import {timeout} from '@azteam/ultilities';
 
 class Provider {
     constructor(configs) {
@@ -7,7 +8,12 @@ class Provider {
     }
     async getConnection(name) {
         if (!this.connections[name]) {
-            this.connections[name] = new RabbitMQAsync(this.configs[name]);
+            const rabbitmq = new RabbitMQAsync(this.configs[name]);
+
+            while (!rabbitmq.connected) {
+                await timeout(1000);
+            }
+            this.connections[name] = rabbitmq;
         }
         return this.connections[name];
     }
